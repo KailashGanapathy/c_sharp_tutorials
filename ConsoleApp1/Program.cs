97afa89b02831db77cd1f7555656540359305c74
Kailash Ganpath <kailashganpath@gmail.com>
2024-06-27 00:08:03 +0530

    changes 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,8 +11,13 @@ using System.Runtime.InteropServices.ComTypes;
 namespace v1
 {
     class Program
-    {
-        static bool Identity_char(char element, int[]countArray)
+    {  
+        /* Funktion zur Berechnung der Anzahl der einzelnen Parts :
+         param[element]: Element aus dem 2D-Array
+         param[countArray]: Array zur Erfassung der Anzahl der Parts 
+         return insert_flag : Boolean flag, um den Index des Good Parts auszugeben.*/
+
+        static bool Identity_char(char element, int[] countArray)
         {
             bool insert_flag = false;
             switch (element)
@@ -38,30 +43,63 @@ namespace v1
         }
         static void Main(string[] args)
         {
-            string filePath = @"D:\swathy_akka\task1\small.MAP";
-            int[] countArray = { 0, 0, 0, 0 };
-            int i = 0, colcount=0;
+            
+            string parentPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            string filePath = null;
 
-            //List<char[]> rows = new List<char[]>();
+            int[] countArray = { 0, 0, 0, 0 }; // Array zur Berechnung der 4 verschiedene Parts (Good, Void,Inked, Reference )
+            int i = 0, colcount = 0;
+
+            List<char[]> rows = new List<char[]>();
+
+            Console.WriteLine("File available: \n 1. small.MAP \n 2. 3H60.MAP \n Enter 1/2: ");
+            //string option = Console.ReadLine();
+            string option = "2";
+            int skipper = 0;
+            int x = 0;
+            Int32.TryParse(option, out x);
+            if (x == 1)
+            {
+                filePath = Path.Combine(parentPath, "small.MAP");
+                skipper = 1;
+            }
+            else if (x == 2)
+            {
+                filePath = Path.Combine(parentPath, "3H60.MAP");
+                skipper = 36;
+            }
+            else
+            {
+                Console.WriteLine("Enter valid option");
+            }
+
             using (StreamReader sr = new StreamReader(filePath))
             {
-                sr.ReadLine(); 
                 string line;
 
+                // Konvertierung der MAP-Datei in ein 2D-Array:
                 while ((line = sr.ReadLine()) != null)
                 {
+                    // Erste Zeile überspringen
+                    if (skipper != 0)
+                    {
+                        skipper--;
+                        continue;
+                    }
                     char[] numbers = new char[line.Length];
-                    for(i=0; i<line.Length; i++)
-                            numbers[i] = line[i];
+                    for (i = 0; i < line.Length; i++)
+                        numbers[i] = line[i];
                     rows.Add(numbers);
                     colcount = line.Length;
                 }
+                //mäanderformig die Datei zu lesen (zig-zag pattern) :
                 for (int col = 0; col < colcount; col++)
                 {
-                    if(col%2 == 0)
+                    if (col % 2 == 0)
                     {
-                        for (int row = 0; row < rows.Capacity; row++)
+                        for (int row = 0; row < rows.Count; row++)
                         {
+                            
                             bool ret = Identity_char(rows[row][col], countArray);
                             if (ret == true)
                                 Console.WriteLine("(" + row + "," + col + ")");
@@ -69,14 +107,16 @@ namespace v1
                     }
                     else
                     {
-                        for (int row = rows.Capacity-1; row >= 0 ; row--)
-                        {   
+                        for (int row = rows.Count - 1; row >= 0; row--)
+                        {
                             bool ret = Identity_char(rows[row][col], countArray);
                             if (ret == true)
-                                Console.WriteLine("("+row+","+col+")");
+                                Console.WriteLine("(" + row + "," + col + ")");
                         }
                     }
                 }
+
+                //Ausgabe :  
                 Console.WriteLine($"Good Parts: {countArray[0]}");
                 Console.WriteLine($"Inked Parts:{countArray[1]}");
                 Console.WriteLine($"Void Parts: {countArray[2]}");
